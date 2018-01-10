9e02c8b707c337622ad0cfb54ce52593375b196f
BenChermenschi <r0614024@student.thomasmore.be>
2018-01-10 14:41:24 +0100

    more cors changes

--- server.js
+++ server.js
@@ -31,8 +31,10 @@ mongoose.connection.on("error",function (err) {
 
 //further setup
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 //app usages
