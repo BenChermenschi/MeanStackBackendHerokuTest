51e1a7bdf48c5a20321b792dfcbc0b679e6e1d03
BenChermenschi <r0614024@student.thomasmore.be>
2018-01-10 11:38:20 +0100

    Revert "cors allow origin ALL"
    This reverts commit f406f8765723ab6c290feb01503a652b2b0933c1.

--- server.js
+++ server.js
@@ -32,11 +32,9 @@ mongoose.connection.on("error",function (err) {
 //further setup
 
 //app usages
-//app.use(cors());
-app.use(cors({origin: '*'}));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
 
 //routes
 var appRoutes = require('./appRoutes');
